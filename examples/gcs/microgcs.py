6a1b1cedbb4edb0782ffe1594d6176e712db3cf0
Yuuya Fujiwara <y.fujiwara@tohotechnos.co.jp>
2018-11-13 13:42:52 +0900

    modiry micro_GCS example.

--- examples/gcs/microgcs.py
+++ examples/gcs/microgcs.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# 日本語コメント使うには先頭に　# coding: UTF-8を書く。
+#
+# 1) cygwinコンソールから、sim_vehicle.pyを実行してSITLを起動する。
+# 2) SITLで起動したMAVProxyコンソールにてポートを追加する。
+#    ※デフォルトポートはMissionPlannerとの接続で使用するため。
+#    $output add 127.0.0.1:14552
+# 3) このコードを実行する。
 """
 © Copyright 2015-2016, 3D Robotics.
 """
@@ -26,9 +33,15 @@ args = parser.parse_args()
 connection_string = args.connect
 sitl = None
 
+# 引数が与えられない場合の接続先
+if not connection_string:
+    connection_string = "127.0.0.1:14552"
+
+# dronekit-sitlに接続
 #Start SITL if no connection string specified
 if not connection_string:
     import dronekit_sitl
+    print("*** not connection string!!! ***" )
     sitl = dronekit_sitl.start_default()
     connection_string = sitl.connection_string()
 
@@ -39,6 +52,12 @@ vehicle = connect(connection_string, wait_ready=True)
 def setMode(mode):
     # Now change the vehicle into auto mode
     vehicle.mode = VehicleMode(mode)
+
+# Arm/Disarmする
+def Arming(state):
+    vehicle.armed = state
+    while not (vehicle.armed == state) and not api.exit:
+        time.sleep(1) 
 
 
 def updateGUI(label, value):
@@ -66,6 +85,11 @@ addObserverAndInit('mode', lambda vehicle,name,mode: updateGUI(modeLabel, mode))
 
 Button(frame, text = "Auto", command = lambda : setMode("AUTO")).pack()
 Button(frame, text = "RTL", command = lambda : setMode("RTL")).pack()
+Button(frame, text = "Guided", command = lambda : setMode("GUIDED")).pack()
+Button(frame, text = "Manual", command = lambda : setMode("MANUAL")).pack()
+Button(frame, text = "Arm", command = lambda : Arming(True)).pack()
+
+
 
 root.mainloop()
 
